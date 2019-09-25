e9b50d499955d5b6725b01f9f2aaafe8eed3cdce
Jennifer Ingram <jennifer.ingram@flatironschool.com>
2019-09-25 17:55:57 -0400

    refactored code to use the heredoc to mimic earlier READMEs

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -12,12 +12,21 @@ class Pokemon
 
     # Here we're inserting a row into our pokemon database (whether or not that pokemon instance has already been initialized)
     def self.save(name, type, db)
-        db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+        sql = <<-SQL
+            INSERT INTO pokemon (name, type) VALUES (?, ?)
+        SQL
+
+        db.execute(sql, name, type)
     end
 
     # Here we're grabbing a row from our pokemon database and making a new instance of pokemon from it
     def self.find(id, db)
-        pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
+        sql = <<-SQL
+            SELECT * FROM pokemon WHERE id = ?
+        SQL
+
+        pokemon = db.execute(sql, id).flatten
+        
         Pokemon.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], db: db)
     end
 end
